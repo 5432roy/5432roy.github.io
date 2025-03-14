78720886caa098b6a6d4830b7c4253fea4dfc9e4
Shang-Yu Chan (Roy) <schan88@ucsc.edu>
2025-03-13 18:44:06 -0700

    `[Add]` Make the right section of the home page more style

--- roy5432-page/src/components/Home.js
+++ roy5432-page/src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     if (typedIndex < fullText.length) {
       const timer = setTimeout(() => {
         setTypedIndex(prev => prev + 1);
-      }, 100); // Adjust speed as desired
+      }, 100);
       return () => clearTimeout(timer);
     }
   }, [typedIndex, fullText]);
@@ -20,22 +20,22 @@ const Home = () => {
       <div className="home-content">
         <div className="home-left">
           <h1>{fullText.slice(0, typedIndex)}</h1>
-          <a className="resume-link" href="https://docs.google.com/document/d/1D9K-XgPMsHHdWQElwrTFZut8oVmc5auJ8HFoLPWuWWc/edit?tab=t.0" target="_blank" rel="noopener noreferrer">
-            View Resume
+          <a className="resume-link" href="https://docs.google.com/document/d/1D9K-XgPMsHHdWQElwrTFZut8oVmc5auJ8HFoLPWuWWc/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+            Resume
           </a>
         </div>
         <div className="home-right">
-          <div className="skills">
-            <h3>Skills</h3>
+          <div className="skills-code-block">
+            <p className="comment"># Skills</p>
             <ul>
-              <li>JavaScript / React</li>
-              <li>HTML / CSS</li>
+              <li>.NET</li>
               <li>Java</li>
               <li>Python</li>
-              <li>C / C++</li>
+              <li>C / C++ / C#</li>
               <li>REST APIs</li>
-              <li>Unit Testing</li>
+              <li>Reverse Engineering</li>
             </ul>
+            <p className='comment'># This page is still under development</p>
           </div>
         </div>
       </div>
