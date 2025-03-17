eb87ec091da2c9b9c8916475b4d89d3399efaade
Shang-Yu Chan (Roy) <schan88@ucsc.edu>
2025-03-17 16:35:22 -0700

    `[Add]` css for the remaining components of the page

--- roy5432-page/src/components/Courses.js
+++ roy5432-page/src/components/Courses.js
@@ -1,51 +1,96 @@
 import React from 'react';
 
 const Courses = () => {
+
+  const ucscCourses = [
+    {
+      title: "CSE 101 Introduction-to-Data-Structures-and-Algorithms",
+      description:
+        "Used C and C++ to build linked lists, red-black trees, and graph ADTs; implemented BFS, DFS, and binary search."
+    },
+    {
+      title: "CSE 120 Computer Architecture",
+      description:
+        "Learned microarchitecture, pipeline, cache, multithreading, and data hazards."
+    },
+    {
+      title: "STAT 131 Introduction to Probability Theory",
+      description:
+        "Covered gamma function, beta distribution, Poisson process, and the central limit theorem."
+    },
+    {
+      title: "CSE 138 Distributed Systems",
+      description:
+        "Covers topics in distributed computing including communication, naming, synchronization, consistency and replication, fault tolerance, and security."
+    }
+  ];
+  
+  const wsuCourses = [
+    {
+      title: "MATH 220 Introductory Linear Algebra",
+      description: ""
+    },
+    {
+      title: "MATH 216 Discrete Structures",
+      description: ""
+    },
+    {
+      title: "CPT_S 233 Advanced Data Structures (Java)",
+      description:
+        "Implemented red-black trees and various sorting algorithms."
+    },
+    {
+      title: "CPT_S 260 Intro to Computer Architecture",
+      description: ""
+    },
+    {
+      title: "CPT_S 317 Automata and Formal Languages",
+      description:
+        "Studied DFA, NFA, context-free grammar, and Turing machines."
+    },
+    {
+      title: "CPT_S 322 Software Engineering I",
+      description:
+        "Collaborated on a project (Studengage) to learn the design cycle and OOP."
+    },
+    {
+      title: "CPT_S 355 Programming Language Design",
+      description: ""
+    },
+    {
+      title: "CPT_S 370 Systems Programming (Java)",
+      description: ""
+    }
+  ];
+  
   return (
-    <section className="section" id="courses">
-      <h2>Taken Courses</h2>
-      <p>*Upper division courses cannot be transferred. Some courses may need to be re-taken.</p>
-      <div>
+<section className="course-section" id='courses'>
+    <h2>Taken Courses</h2>
+    <div className="course-content">
+      <div className="course-ucsc course-list">
         <h3>At University of California Santa Cruz</h3>
         <ul>
-          <li>
-            <a
-              href="https://github.com/5432roy/Introduction-to-Data-Structures-and-Algorithms"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              CSE 101 Introduction-to-Data-Structures-and-Algorithms
-            </a>
-            : Used C and C++ to build linked lists, red-black trees, and graph ADTs; implemented BFS, DFS, and binary search.
-          </li>
-          <li>
-            CSE 120 Computer Architecture: Learned microarchitecture, pipeline, cache, multithreading, and data hazards.
-          </li>
-          <li>
-            STAT 131 Introduction to Probability Theory: Covered gamma function, beta distribution, Poisson process, and the central limit theorem.
-          </li>
+          {ucscCourses.map((course, index) => (
+            <li key={index}>
+              <strong>{course.title}</strong>
+              {course.description && `: ${course.description}`}
+            </li>
+          ))}
         </ul>
       </div>
-      <div>
+      <div className="course-wsu course-list">
         <h3>At Washington State University</h3>
         <ul>
-          <li>MATH 220 Introductory Linear Algebra</li>
-          <li>MATH 216 Discrete Structures</li>
-          <li>
-            CPT_S 233 Advanced Data Structures (Java): Implemented red-black trees and various sorting algorithms.
-          </li>
-          <li>CPT_S 260 Intro to Computer Architecture</li>
-          <li>
-            CPT_S 317 Automata and Formal Languages: Studied DFA, NFA, context-free grammar, and Turing machines.
-          </li>
-          <li>
-            CPT_S 322 Software Engineering I: Collaborated on a project (Studengage) to learn the design cycle and OOP.
-          </li>
-          <li>CPT_S 355 Programming Language Design</li>
-          <li>CPT_S 370 Systems Programming (Java)</li>
+          {wsuCourses.map((course, index) => (
+            <li key={index}>
+              <strong>{course.title}</strong>
+              {course.description && `: ${course.description}`}
+            </li>
+          ))}
         </ul>
       </div>
-    </section>
+    </div>
+  </section>
   );
 };
 
